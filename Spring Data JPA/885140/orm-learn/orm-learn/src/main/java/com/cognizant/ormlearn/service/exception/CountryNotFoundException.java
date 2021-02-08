7c1199d13ced8c93b633ebe4aaabd64393177ba9
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-08 12:25:57 +0530

    Delete Spring Data JPA/885140 directory

--- Spring Data JPA/885140/orm-learn/orm-learn/src/main/java/com/cognizant/ormlearn/service/exception/CountryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.ormlearn.service.exception;
-
-public class CountryNotFoundException extends Exception{
-	
-	public CountryNotFoundException(String message) {
-		super(message);
-	}
-
-}
