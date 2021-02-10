a71864bc633d832260593f3ead7e42e289425a30
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-10 18:01:13 +0530

    Add files via upload

---
+++ Spring Data JPA/885108/Day 1/HandsOn-1/src/com/model/Address.java
@@ -0,0 +1,38 @@
+package com.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+	private String city;
+	private String state;
+	private String street;
+	private String pincode;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	
+}
