e01fbfee5328ac359171ca2fb90277aeb2947252
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-11 13:55:01 +0530

    Add files via upload

---
+++ Spring Data JPA/885147/Spring Data JPA Final Check/moviecruiser/src/test/java/com/cognizant/moviecruiser/MoviecruiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.moviecruiser;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecruiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
