a492b1a0cad14632f2e0c7ca51fbf55ef3c14f30
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-17 20:43:05 +0530

    Add files via upload

---
+++ Data Structure/885114/day 3/Find the Running Median/Solution.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the runningMedian function below.
+     */
+    static double[] runningMedian(int[] a) {
+        /*
+         * Write your code here.
+         */
+        double[] result = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            addNumber(a[i]);
+            // System.out.println(getMedian());
+            result[i] = getMedian();
+        }
+
+        return result;
+    }
+
+    private static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // keeps track of
+    // the SMALL
+    // numbers
+    private static PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // keeps track of the LARGE numbers
+
+    private static void addNumber(int n) {
+        if (maxHeap.isEmpty()) {
+            maxHeap.add(n);
+        } else if (maxHeap.size() == minHeap.size()) {
+            if (n < minHeap.peek()) {
+                maxHeap.add(n);
+            } else {
+                minHeap.add(n);
+                maxHeap.add(minHeap.remove());
+            }
+        } else if (maxHeap.size() > minHeap.size()) {
+            if (n > maxHeap.peek()) {
+                minHeap.add(n);
+            } else {
+                maxHeap.add(n);
+                minHeap.add(maxHeap.remove());
+            }
+        }
+        // maxHeap will never have fewer elements than minHeap
+    }
+
+    private static double getMedian() {
+        if (maxHeap.isEmpty()) {
+            return 0;
+        } else if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else { // maxHeap must have more elements than minHeap
+            return maxHeap.peek();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int aCount = Integer.parseInt(scanner.nextLine().trim());
+
+        int[] a = new int[aCount];
+
+        for (int aItr = 0; aItr < aCount; aItr++) {
+            int aItem = Integer.parseInt(scanner.nextLine().trim());
+            a[aItr] = aItem;
+        }
+
+        double[] result = runningMedian(a);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bufferedWriter.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+    }
+}
