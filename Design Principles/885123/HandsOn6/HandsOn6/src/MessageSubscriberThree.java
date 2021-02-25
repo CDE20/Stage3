f2c8128d10d85391a6293489134e0df52fbb0154
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-25 13:48:36 +0530

    Add files via upload

---
+++ Design Principles/885123/HandsOn6/HandsOn6/src/MessageSubscriberThree.java
@@ -0,0 +1,8 @@
+public class MessageSubscriberThree implements Observer {
+	@Override
+	public void update(Message m) {
+
+		System.out.println("MessageSubscriberThree :: " + m.getMessageContent());
+
+	}
+}
---
+++ Design Principles/885123/HandsOn6/HandsOn6/src/Subject.java
@@ -0,0 +1,9 @@
+
+public interface Subject {
+	public void attach(Observer o);
+
+	public void detach(Observer o);
+
+	public void notifyUpdate(Message m);
+
+}
