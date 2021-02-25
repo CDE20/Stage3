eac48c3cbc1f558a36074db854d99cb4761b13ef
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-25 12:35:17 +0530

    Add files via upload

---
+++ Design Principles/885025/Day - 2 Hands on/05 - 03 - DP - Hands on/Builder Design Pattern/src/Pepsi.java
@@ -0,0 +1,12 @@
+public class Pepsi extends ColdDrink {
+
+   @Override
+   public float price() {
+      return 30.0f;
+   }
+
+   @Override
+   public String name() {
+      return "Pepsi";
+   }
+}
---
+++ Design Principles/885025/Day - 2 Hands on/05 - 04 - DP - Hands on/Facade Design Pattern/src/Circle.java
@@ -0,0 +1,10 @@
+
+public class Circle implements Shape{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing circle");
+	}
+
+}
