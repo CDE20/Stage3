ebcde3d3d7b97133e1ccafbedc5cda12a4ace652
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-25 15:47:33 +0530

    Add files via upload

---
+++ Design Principles/885114/Day 1 - Session 1/SOLID Principle/src/com/cognizant/interfaces/IOrderRepair.java
@@ -0,0 +1,9 @@
+package com.cognizant.interfaces;
+
+public interface IOrderRepair {
+	void processOrder(String modelName);
+
+	void processPhoneRepair(String modelName);
+
+	void processAccessoryRepair(String accessoryType);
+}
