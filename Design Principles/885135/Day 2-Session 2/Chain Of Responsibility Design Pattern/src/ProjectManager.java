957f097e5b4efe7e48dd89e14200998ae430fd89
IJ006 <75236474+IJ006@users.noreply.github.com>
2021-02-25 11:07:19 +0530

    Add files via upload

---
+++ Design Principles/885135/Day 2-Session 2/Chain Of Responsibility Design Pattern/src/ProjectManager.java
@@ -0,0 +1,7 @@
+
+public class ProjectManager extends ILeaveRequestHandler{
+	
+	public ProjectManager(int days) {
+		this.days=days;
+	}
+}
---
+++ Design Principles/885135/Day 2-Session 2/Facade Design Pattern/src/Square.java
@@ -0,0 +1,10 @@
+
+public class Square implements Shape{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing square");
+	}
+
+}
